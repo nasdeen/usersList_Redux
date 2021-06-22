9797cb796e760fb94d1b71a020e33422a7ff2d38
2known <61994491+nasdeen@users.noreply.github.com>
2021-06-22 19:08:49 +0000

    config file for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQXkJi07u7BeSPJbMucPupZXLyti5bO8U",
+  authDomain: "mienu-73186.firebaseapp.com",
+  projectId: "mienu-73186",
+  storageBucket: "mienu-73186.appspot.com",
+  messagingSenderId: "980984310182",
+  appId: "1:980984310182:web:924a48d6364c9d63f08767",
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;
