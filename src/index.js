47c25854ee6a58a84f3889486d1f93314d31cef8
2known <61994491+nasdeen@users.noreply.github.com>
2021-06-22 19:16:09 +0000

    update on import of firebase, firestore, compose

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import firebase from "./firebase/config";
+import { getFirebase, reduxReactFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import usersReducer from "./Store/usersReducer";
 import Router from "./Router";
 
-const store = createStore(usersReducer, applyMiddleware(thunk));
+const store = createStore(
+  usersReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+    reduxReactFirebase(firebase),
+    reduxFirestore(firebase)
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
